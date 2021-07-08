9aaa64b213d5c2d7a976979edbdf0832015e83c3
PRO1_event <vosong9201@gmail.com>
2021-07-08 17:46:33 +0700

    login JWT huy

---
+++ src/main/java/com/TTS/DTO/ResponseLogin.java
@@ -0,0 +1,8 @@
+package com.TTS.DTO;
+
+import com.TTS.sercurity.JWT.JWTtoken;
+
+public class ResponseLogin {
+private String token;
+private JWTtoken hmm;
+}
--- src/main/java/com/TTS/Java661Application.java
+++ src/main/java/com/TTS/Java661Application.java
@@ -1,15 +1,18 @@
 package com.TTS;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.TTS.sercurity.DomainUserDetailsService;
 
 @SpringBootApplication
 //@EnableJpaRepositories
 public class Java661Application {
-
+	private final Logger _log = Logger.getLogger(DomainUserDetailsService.class);
 	public static void main(String[] args) {
 		SpringApplication.run(Java661Application.class, args);
+		
 	}
 
 }
